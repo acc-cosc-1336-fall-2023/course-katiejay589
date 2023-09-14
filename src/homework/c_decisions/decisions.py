71812287fe1792754ff2c54ae1fe4703fe0906a2
Katelyn Jenkins <137010784+KatieMJenkins@users.noreply.github.com>
2023-09-14 15:51:20 +0000

    # if/elif decisions homework

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,18 @@
-#
+def get_options_ratio (option, total):
+    options_ratio = option /total
+    return options_ratio
+    
+def get_faculty_rating(options_ratio):
+    faculty_rating =  ""
+    if (options_ratio >= 0.9 and options_ratio <1.0):
+        faculty_rating= "Excellent"
+    elif (options_ratio >= 0.8 and options_ratio <0.9):
+        faculty_rating = "Very Good"
+    elif (options_ratio >= 0.7 and options_ratio <0.8):
+        faculty_rating = "Good"
+    elif (options_ratio >= 0.6 and options_ratio <0.7):
+        faculty_rating = "Needs Improvement"
+    elif (options_ratio >= 0.0 and options_ratio <0.6):
+        faculty_rating = "Unacceptable"
+    return faculty_rating
+       
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,9 @@
-#
+import decisions
+#prompt user for information and use the input to get a faculty rating
+
+option = int(input("Enter the numerical grade"))
+
+total = int(input("Enter total possible grade"))
+result=decisions.get_options_ratio(option, total)
+
+print ("The Faculty Rating is " + decisions.get_faculty_rating(result))
