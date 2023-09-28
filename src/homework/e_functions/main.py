dea9279a6da778398e247c701c5f38e09048ac3f
Katelyn Jenkins <137010784+KatieMJenkins@users.noreply.github.com>
2023-09-28 18:25:49 +0000

    #value return functions, f string format hmwk 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,13 @@
-#
+# get epoch time for input 3800 seconds
+
+import value_return
+
+result_1 = value_return.get_hour(3800)
+
+result_2 = value_return.get_minutes(3800)
+
+result_3 = value_return.get_seconds(3800)
+
+stuff_in_string = f"time is {result_1:02d}:{result_2:02d}:{result_3:02d}"
+
+print(stuff_in_string)
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,16 @@
-#
+#function that can calculate the current number of hours since 1970 if given epoch time in seconds
+
+def get_hour(epoch_seconds):
+    hour = (epoch_seconds//3600)
+    return hour
+
+def get_minutes(epoch_seconds):
+    remainder_seconds = (epoch_seconds%3600)
+    minutes = (remainder_seconds//60)
+    return minutes
+
+def get_seconds(epoch_seconds):
+    remainder1 = (epoch_seconds%3600)
+    seconds= round(remainder1%60)
+    return seconds
+  
